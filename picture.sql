e066f0bcf25b870703bb9c84bbf85fbd54c1937f
haiwenyang <39043128+haiwenyang@users.noreply.github.com>
2018-07-10 20:54:17 +0800

    sql语句
    sql语句

---
+++ picture.sql
@@ -0,0 +1,17 @@
+﻿INSERT INTO `picture` VALUES ('201665231020.jpg', '科比1');
+INSERT INTO `picture` VALUES ('20166523106.jpg', '科比2');
+INSERT INTO `picture` VALUES ('20166523217.jpg', '科比3');
+INSERT INTO `picture` VALUES ('201665233736.jpg', '多少点');
+INSERT INTO `picture` VALUES ('201665233929.jpg', 'cjj');
+INSERT INTO `picture` VALUES ('2016652362.jpg', '科比4');
+INSERT INTO `picture` VALUES ('20166523945.jpg', '科比5');
+INSERT INTO `picture` VALUES ('20166523956.jpg', '科比6');
+INSERT INTO `picture` VALUES ('20166602250.jpg', '科比');
+INSERT INTO `picture` VALUES ('20166602846.jpg', '罗斯');
+INSERT INTO `picture` VALUES ('20166602928.jpg', '科比');
+INSERT INTO `picture` VALUES ('20166604826.jpg', '螺丝');
+
+
+
+E:\appserv\MySQL\bin\mysqld --defaults-file=E:\appserv\MySQL\my.ini mysql
+C:\xampp\mysql\bin\mysqld --defaults-file=C:\xampp\mysql\my.ini mysql
